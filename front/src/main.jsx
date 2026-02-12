28dd61027bf14a4494b8a3ff16ceceb279e0fb00
João Silva <joao.silva-coutinho@laplateforme.io>
2026-02-12 10:36:56 +0100

    Revert "66 reservation responsive"

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -21,8 +21,6 @@ import Evennements from "./pages/Evennements.jsx";
 import Upload from "./pages/public/Upload.jsx";
 import Palmares from "./pages/public/Palmares.jsx";
 import Agenda from "./pages/public/Agenda.jsx";
-import Reservation from "./pages/public/Reservation.jsx";
-
 
 
 const queryClient = new QueryClient({
@@ -50,9 +48,6 @@ createRoot(document.getElementById("root")).render(
             <Route path="/upload" element={<Upload />} />
             <Route path="/palmares" element={<Palmares />} />
             <Route path="/agenda" element={<Agenda />} />
-            <Route path="/reservation" element={<Reservation />} />
-
-            
           </Route>
 
           {/* Routes privées */}
