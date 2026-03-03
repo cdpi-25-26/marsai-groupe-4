ad8c8c6c96eb15ee951bcb56d2c345c203106d56
PolinaDubova <pharabadot@gmail.com>
2026-03-03 09:15:46 +0100

    palmares fini

--- back/src/models/associations.js
+++ back/src/models/associations.js
@@ -1,8 +1,8 @@
 import User from "./User.js";
 import Film from "./Video.js";
+import Award from "./Award.js";  
 import FilmsJury from "./FilmsJury.js";
 
-// Define all associations here after all models are loaded
 export function setupAssociations() {
   // Film -> User (belongsTo)
   Film.belongsTo(User, { foreignKey: "user_id", as: "user" });
@@ -25,4 +25,8 @@ export function setupAssociations() {
   // FilmsJury associations
   FilmsJury.belongsTo(Film, { foreignKey: "film_id", as: "film" });
   FilmsJury.belongsTo(User, { foreignKey: "user_id", as: "jury" });
+
+ 
+  Award.belongsTo(Film, { foreignKey: "film_id" });  
+  Film.hasMany(Award, { foreignKey: "film_id" });
 }
