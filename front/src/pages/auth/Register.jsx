ab0677e7f48ffd8710542b78a9df9f855c0cc5d0
karim-sebih <karim.sebih@laplateforme.io>
2026-03-06 11:26:18 +0100

    contenir au moins 8 caractères au lieux de 6

--- front/src/pages/auth/Register.jsx
+++ front/src/pages/auth/Register.jsx
@@ -16,7 +16,7 @@ const registerSchema = z
     email: z.string().email("Email invalide"),
     password: z
       .string()
-      .min(6, "Le mot de passe doit contenir au moins 6 caractères"),
+      .min(8, "Le mot de passe doit contenir au moins 8 caractères"),
     confirmpassword: z.string(),
   })
   .refine((data) => data.password === data.confirmpassword, {
