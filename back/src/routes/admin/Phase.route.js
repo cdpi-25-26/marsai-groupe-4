de88041d1db24e48f7cb7f48444f1d639722cc3d
karim-sebih <karim.sebih@laplateforme.io>
2026-03-10 12:39:59 +0100

    post top50

--- back/src/routes/admin/Phase.route.js
+++ back/src/routes/admin/Phase.route.js
@@ -6,7 +6,7 @@ const Phaserouter = express.Router();
 
 // Phaserouter.get("/phase1",PhaseController.getPhases1Video);
 Phaserouter.get("/phase1", (req, res, next) => {  AuthMiddleware(req, res, next, ["ADMIN", "JURY","PRODUCER"]);}, PhaseController.getPhases1Video);
-Phaserouter.get("/top50",  PhaseController.getTop50);
+Phaserouter.post("/top50",  PhaseController.getTop50);
 Phaserouter.put("/prize/:id", PhaseController.assignPrize);
 
 export default Phaserouter;
