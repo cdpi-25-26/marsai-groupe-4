7a85c8cd087962a3fa89ba902131bf217901947b
v4he <vahe.vardanyan@laplateforme.io>
2026-03-12 16:26:15 +0100

    cms CSS, dashboard sidebar

--- front/src/components/admin/AdminSideBar.jsx
+++ front/src/components/admin/AdminSideBar.jsx
@@ -54,7 +54,7 @@ export default function AdminSidebar() {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <NavLink to="/admin">
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
+                <div className="flex aspect-square size-8 m-[-8px] items-center justify-center rounded-lg bg-primary text-primary-foreground">
                   <LayoutDashboard className="size-4" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
